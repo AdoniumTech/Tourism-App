4d9223f25cab49201beb6d05ec45db4f30a8241a
Atikonda Katundu <atikondakatundu@gmail.com>
2026-01-16 14:35:11 +0200

    made an example page

---
+++ src/example.jsx
@@ -0,0 +1,7 @@
+export default function Example() {
+    return (
+        <div className="w-[100vw] h-[100vh] flex items-center justify-center bg-green-300">
+            <h1>Example</h1>
+        </div>
+    )
+}
